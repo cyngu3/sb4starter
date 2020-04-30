dcded97c1dc44c40f13653e83b3895926ef67440
cyngu3mod <cyngu3@gmail.com>
2020-04-30 22:52:00 +0900

    프로젝트 생성 및 기본적인 세팅 완료

---
+++ src/main/java/com/sbs/starter/SrarterApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrarterApplication.class, args);
+	}
+
+}
