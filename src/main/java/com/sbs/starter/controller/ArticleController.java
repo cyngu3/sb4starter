67a5d3eba0316de3046fc8eb1ab7dfc399694e7f
cyngu3 <cyngu3@gmail.com>
2020-05-01 20:41:57 +0900

    ArticleDAOImpl삭제하고 mybatis에서 처리한 직후 임

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sbs.starter.dto.ArticleDTO;
+import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +18,8 @@ public class ArticleController {
 	ArticleService articleService;
 	
 	@RequestMapping("/article/list")
-	//@ResponseBody ////직접 "메인화면입니다"가 나온다:
 	public String showList(Model aModel) {	//처음에는 ("") 이었음: 
-		List<ArticleDTO> list = articleService.getList(); 
-		//log.info("list : "+ list); //확인용으로 쓰고서 닫음
+		List<Article> list = articleService.getList(); 
 		aModel.addAttribute("list", list);
 		//request.setAttribute("list", list)와 똑같은 표현!!
 		return "article/list";   ////@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
--- src/main/webapp/WEB-INF/jsp/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/article/list.jsp
@@ -3,7 +3,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>    
 <!-- 아직 아님:: <c:set var="pageName" value="게시물 리스트" /> -->   
 <!-- <%@ page import="java.util.*" %> -->
-<!-- <%@ page import="com.sbs.starter.dto.ArticleDTO" %> -->    
+<!-- <%@ page import="com.sbs.starter.dto.Article" %> -->    
 
 <!DOCTYPE html>
 <html>
@@ -14,7 +14,7 @@
 <body>
 	<h1>게시물 리스트</h1>
 	<h2>v2</h2>
-	<!-- el의 장점은  request.setAttribute("articleDTO") 할 필요가 없다 -->
+	<!-- el의 장점은  request.setAttribute("article") 할 필요가 없다 -->
 	<!-- 참고: "게시물 리스트를 자바버전(el)(나중에 제거 함), jstl 버전 2가지로 생성 되었었음  -->
 	<c:forEach items="${list}" var="article">
 		<section>번호 : ${article.id}, 제목: ${article.title}, 내용: ${article.body} </section>
