a3cdf6b190a4d480543105c0a7a88494f3876645
cyngu3 <cyngu3@gmail.com>
2020-05-01 17:42:26 +0900

    게시물을 list.jsp에서 자바버전el 과, jstl버전(taglib)두가지 중 jstl로 처리함

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -3,10 +3,10 @@ package com.sbs.starter.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sbs.starter.dto.Article;
+import com.sbs.starter.dto.ArticleDTO;
 import com.sbs.starter.service.ArticleService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +19,11 @@ public class ArticleController {
 	
 	@RequestMapping("/article/list")
 	//@ResponseBody ////직접 "메인화면입니다"가 나온다:
-	public String showList() {
-		List<Article> list = articleService.getList(); 
-		
-		log.info("list : "+ list); //
-		
+	public String showList(Model aModel) {	//처음에는 ("") 이었음: 
+		List<ArticleDTO> list = articleService.getList(); 
+		//log.info("list : "+ list); //확인용으로 쓰고서 닫음
+		aModel.addAttribute("list", list);
+		//request.setAttribute("list", list)와 똑같은 표현!!
 		return "article/list";   ////@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
 	}
 }
