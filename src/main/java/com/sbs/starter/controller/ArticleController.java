a9ce3cb60b661f173ecf6fa0d9794a3beff6bc68
cyngu3 <cyngu3@gmail.com>
2020-05-02 16:42:32 +0900

    doAdd 처리한 직후 in_ useGeneratedKey

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,10 +1,14 @@
 package com.sbs.starter.controller;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
@@ -17,17 +21,23 @@ public class ArticleController {
 	@Autowired		//:@Service를 만나서 진행:
 	ArticleService articleService;
 	
-	@RequestMapping("/article/list")
+	@RequestMapping("/article/list") ///@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
 	public String showList(Model aModel) {	//초기에는 ("") 이었음: 
 		List<Article> list = articleService.getList(); 
 		aModel.addAttribute("list", list);
 		//request.setAttribute("list", list)와 똑같은 표현!!
-		return "article/list";   ////@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
+		return "article/list";
 	}
 	
 	@RequestMapping("/article/add")
 	public String showAdd() {	
-
-		return "article/add";   ////@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
+		return "article/add"; //add(form)이다  
+	}
+	
+	@RequestMapping("/article/doAdd") //=>articleDAO =>articleService 에서 처리한다
+	@ResponseBody   //@ResponseBody일 경우 : 직접 "메인화면입니다"가 나온다.
+	public String doAdd(@RequestParam Map<String, Object> param) { //String title, String Body
+		articleService.add(param);  //long newId = articleService.add(param);
+		return "게시물이 추가되었습니다"; //"article/add";   //
 	}
 }
